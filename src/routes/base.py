d776d89391db16f22013087a68749d7f97382d20
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-06-07 12:43:49 +0300

    update base.py with starting call and get_settings to recongnize the .env configurations

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 import os
-from helpers.config import get_settings
+from helpers.config import get_settings, Settings
 
 
 
@@ -10,8 +10,8 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome():
-    app_settings = get_settings()
+async def welcome(app_settings: Settings = Depends(get_settings)):
+#    app_settings = get_settings()
     
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
